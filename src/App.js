7bce3af777f074c610dce9b09a97f02de92b3859
zuberqureshi <zuberqureshi95@gmail.com>
2021-09-21 02:50:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import logo from "./logo.svg";
+import "./App.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import Home from "./components/Home";
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
+function App() {
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg} bbgnghn</span>
-      </p>
-    )
-  }
+        <div className="App">
+            <Home />
+        </div>
+    );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
